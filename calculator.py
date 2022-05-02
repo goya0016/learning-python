8376cbb06d4290d8d0d46ff0e8af5a146363c108
goya0016 <goya0016@algonquinlive.com>
2022-05-02 04:09:22 -0400

    half day 1

---
+++ calculator.py
@@ -0,0 +1,14 @@
+#basic started
+
+# x=input("value")
+# y=input("value")
+# z= int(x)+int(y)
+# print (z)
+
+
+#simplified
+x=int(input("value"))
+y=(input("value"))
+
+#addition
+print (x+y)
--- hello.py
+++ hello.py
@@ -1,1 +1,22 @@
-print("hello world")
+# get input from user
+name = input("Your name? ").strip().title()
+
+# remove whitespace from string
+# name = name.strip()
+
+# # captialize first letter
+# name = name.capitalize()
+
+# # captialize whole
+# name = name.title()
+
+# # combine
+# name = name.strip().title()
+
+
+
+# print
+print("hello,", "\"name\"")
+print(f"hello, {name}")
+print("hello,",name)
+print("hello, "+name)
