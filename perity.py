e515062e8588b2600073d1257f2cb6267b8c897c
goya0016 <goya0016@algonquinlive.com>
2022-05-02 23:28:53 -0400

    if's

--- perity.py
+++ perity.py
@@ -7,7 +7,9 @@ def main():
  else: print('odd')
 
 def is_even(n):
-    if n%2 == 0: return True
-    else: return False
+    # if n%2 == 0: return True
+    # else: return False
 
+    # return True if n % 2 == 0 else False
+    return n % 2 == 0 
 main()
