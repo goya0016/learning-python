48acf157eeec09231b5ed56f84c0f90a74df032f
goya0016 <goya0016@algonquinlive.com>
2022-05-02 22:59:09 -0400

    or and else conditionals

--- conditionals.py
+++ conditionals.py
@@ -1,9 +1,19 @@
 x= int(input("value of x "))
 y= int(input("value of y "))
 
-if x<y:
-    print("x is less than y")
-if x>y:
-    print("y is less than x")
-if x==y:
-    print ("x = y")
+# if x<y:
+#     print("x is less than y")
+# elif x>y:
+#     print("y is less than x")
+# else:
+#     print ("x = y")
+
+# if x<y or x>y :
+#     print('x not equals y')
+# else:
+#     print('x equals y')
+
+if x!=y:
+        print('x not equals y')
+else:
+        print('x equals y')
